bbc3702cbc4687beabb64e4c98604cb9349b6e0d
Grinchs <marcisg2345@gmail.com>
2023-12-05 21:10:18 +0200

    Add project files.

---
+++ 10_Masivi_3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_Masivi_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = new int[10];
+
+            Random rnd = new Random();
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = rnd.Next(1, 21); 
+            }
+
+            
+            
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.WriteLine($"mas[{i + 1}] = {mas[i]} ");
+            }
+
+            
+            int min = mas[0];
+            int max = mas[0];
+
+            for (int i = 1; i < mas.Length; i++)
+            {
+                if (mas[i] < min)
+                {
+                    min = mas[i];
+                }
+                else if (mas[i] > max)
+                {
+                    max = mas[i];
+                }
+            }
+
+            Console.WriteLine("\n\nMax vērtība: " + max);
+            Console.WriteLine("Min vērtība: " + min);
+        }
+    }
+}
